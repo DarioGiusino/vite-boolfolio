b3b16f21288d7a9191e9235970a8516a7d2d4a53
DarioGiusino <dario.giusino@gmail.com>
2023-03-16 17:11:35 +0100

    registered new route for detailPage

---
+++ src/pages/ProjectDetailPage.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: 'ProjectDetailPage'
+}
+</script>
+
+<template>
+    <section id="project-details">
+        <h1>ciaone</h1>
+    </section>
+</template>
+
+<style></style>
