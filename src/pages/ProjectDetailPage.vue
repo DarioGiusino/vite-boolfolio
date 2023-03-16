b1abf4ad9fd76c49f9141021f01452bca577e875
DarioGiusino <dario.giusino@gmail.com>
2023-03-16 17:26:03 +0100

    styled detail page

--- src/pages/ProjectDetailPage.vue
+++ src/pages/ProjectDetailPage.vue
@@ -6,8 +6,28 @@ export default {
 
 <template>
     <section id="project-details">
-        <h1>ciaone</h1>
+        <!-- project title -->
+        <h1 class="text-center mb-5">Titolo progetto</h1>
+        <!-- project image -->
+        <div class="img-box mb-5"></div>
+        <!-- project content -->
+        <p>owinonwocnwnei</p>
+        <!-- project button&links -->
+        <div class="project-buttons">
+            <a href="">ciaone</a>
+        </div>
     </section>
 </template>
 
-<style></style>
+<style scoped lang="scss">
+.img-box {
+    border: 2px solid rebeccapurple;
+    border-radius: 25px;
+    overflow: hidden;
+    height: 30rem;
+    background-image: url("https://marcolanci.it/utils/placeholder.jpg");
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+}
+</style>
