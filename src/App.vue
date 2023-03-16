84bc02a194bec1132c318a136c173774b421bdb2
DarioGiusino <dario.giusino@gmail.com>
2023-03-16 15:02:15 +0100

    called api

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,23 @@
 <script>
+import axios from 'axios';
+const baseApiUrl = 'http://127.0.0.1:8000/api/';
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      projects: []
+    }
+  },
+  methods: {
+    fetchProjects() {
+      axios.get(baseApiUrl + 'projects').then(res => {
+        this.projects = res.data;
+      })
+    }
+  },
+  created() {
+    this.fetchProjects();
+  }
 }
 </script>
 
