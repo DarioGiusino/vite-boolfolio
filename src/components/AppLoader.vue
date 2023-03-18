0b6b47449c70149acf1130fed422d43949a62498
DarioGiusino <dario.giusino@gmail.com>
2023-03-18 17:50:35 +0100

    created appLoader

---
+++ src/components/AppLoader.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+    name: 'AppLoader',
+}
+</script>
+
+<template>
+    <div class="loader-overlay">
+        <div class="spinner-boarder" role="status"></div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.loader-overlay {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    top: 0;
+    right: 0;
+    z-index: 1;
+
+    background-color: rgba(0, 0, 0, 0.5);
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    .spinner-boarder {
+        width: 250px;
+        height: 250px;
+        color: #fff;
+    }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { createApp } from "vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { router } from "./router";
 import App from "./App.vue";
+import AppLoader from "./components/AppLoader.vue";
 
 const app = createApp(App);
+app.component("AppLoader", AppLoader);
 app.use(router);
 
 app.mount("#app");
