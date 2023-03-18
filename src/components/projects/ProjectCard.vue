ac5fd689e88f595009ea7137ec14ce807a316df9
DarioGiusino <dario.giusino@gmail.com>
2023-03-18 17:11:06 +0100

    inserted type and technologies in project card

--- src/components/projects/ProjectCard.vue
+++ src/components/projects/ProjectCard.vue
@@ -1,7 +1,12 @@
 <script>
 export default {
     name: 'ProjectCard',
-    props: { project: Object }
+    props: { project: Object },
+    computed: {
+        abstract() {
+            return this.project.description = this.project.description.substring(0, 150) + '...';
+        }
+    }
 }
 </script>
 
@@ -11,11 +16,16 @@ export default {
             <img src="https://marcolanci.it/utils/placeholder.jpg" class="card-img-top" alt="...">
             <div class="card-body d-flex flex-column justify-content-between">
                 <div>
-                    <h5 class="card-title">{{ project.title }} <sup>type</sup></h5>
-                    <p class="card-text">{{ project.description }}[abstract]</p>
+                    <h6 class="card-title">{{ project.title }} <sup><span class="badge rounded-pill"
+                                :style="`background-color: ${project.type.color}`">{{ project.type.label }}</span></sup>
+                    </h6>
+                    <p class="card-text">{{ abstract }}</p>
                     <div class="mb-2">
                         <h6 class="card-title">Used technologies:</h6>
-                        <div>rounded pills</div>
+                        <div><sup><span v-for="technologies in project.technologies" :key="technologies.id"
+                                    class="badge rounded-pill me-1 mb-1"
+                                    :style="`background-color: ${technologies.color}`">{{
+                                        technologies.label }}</span></sup></div>
                     </div>
                 </div>
                 <div class="d-flex justify-content-between align-items-center">
