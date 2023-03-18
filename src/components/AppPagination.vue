cbf9b51f274fe52e0c2905c3acb077164e6c2931
DarioGiusino <dario.giusino@gmail.com>
2023-03-18 19:19:14 +0100

    completed pagination

---
+++ src/components/AppPagination.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: 'AppPagination',
+    props: { links: Array },
+    emits: ['change-page']
+}
+</script>
+
+<template>
+    <nav>
+        <ul class="pagination">
+            <li v-for="link in links" :key="link.label" class="page-item"
+                :class="[{ 'active': link.active }, { 'disabled': !link.url }]">
+                <button type="button" :disabled="!link.url" class="page-link" v-html="link.label"
+                    @click="$emit('change-page', link.url)"></button>
+            </li>
+        </ul>
+    </nav>
+</template>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,22 +1,30 @@
 <script>
 import axios from 'axios';
 import ProjectsList from '../components/projects/ProjectsList.vue';
+import AppPagination from '../components/AppPagination.vue';
 const baseApiUrl = 'http://127.0.0.1:8000/api/';
 
 export default {
     name: 'HomaPage',
-    components: { ProjectsList },
+    components: { ProjectsList, AppPagination },
     data() {
         return {
-            projects: [],
+            projects: {
+                data: [],
+                links: []
+            },
             isLoading: false
         }
     },
     methods: {
-        fetchProjects() {
+        fetchProjects(endpoint = null) {
             this.isLoading = true;
-            axios.get(baseApiUrl + 'projects').then(res => {
-                this.projects = res.data;
+
+            if (!endpoint) endpoint = baseApiUrl + 'projects';
+
+            axios.get(endpoint).then(res => {
+                const { data, links } = res.data;
+                this.projects = { data, links };
                 this.isLoading = false;
             }).catch(err => {
                 console.error(err);
@@ -34,7 +42,10 @@ export default {
 
 <template>
     <AppLoader v-if="isLoading" />
-    <ProjectsList v-else :projects="projects" />
+    <div v-else>
+        <ProjectsList :projects="projects.data" />
+        <AppPagination :links="projects.links" @change-page="fetchProjects" />
+    </div>
 </template>
 
 <style></style>
